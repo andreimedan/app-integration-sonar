7ecdb6293379ba24c4bee1c48d2584122b3a3a39
andreimedan <andrei.medan@yahoo.com>
2022-05-19 12:14:47 +0300

    Initial commit

---
+++ src/main/java/com/am/appintegrationsonar/AppIntegrationSonarApplication.java
@@ -0,0 +1,13 @@
+package com.am.appintegrationsonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppIntegrationSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppIntegrationSonarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/am/appintegrationsonar/AppIntegrationSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.am.appintegrationsonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppIntegrationSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
